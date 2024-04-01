75fc6184a788116c916c1f1ffe755d2d903e1618
Trong Nguyen <nguyentrong101094@gmail.com>
2024-04-01 16:00:35 +0700

    delete current additional ad placement

--- AdsManager Example/AdditionAdPlacementID.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public partial class AdPlacement
-{
-    public static readonly Type RewardedInterstitial = 100;
-    public static readonly Type Reward2 = 101;
-}
